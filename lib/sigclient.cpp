0e1f24eb600bf8fd551a1b3ebef4ed9f67461572
IJR <ijr@musicbrainz.org>
2000-10-02 13:02:51 +0000

    - fix the proxy stuff
    git-svn-id: http://svn.musicbrainz.org/libmusicbrainz/trunk@329 b0b80210-5d09-0410-99dd-b4bd03f891c0

--- lib/sigclient.cpp
+++ lib/sigclient.cpp
@@ -131,7 +131,7 @@ int SigClient::Connect(string& strIP, int nPort)
         m_pSocket->SetProxy(NULL);
     else {
         char *proxyurl = new char[m_proxyAddr.size() + 128];
-        sprintf(proxyurl, "http://%s:%d", m_proxyAddr.c_str(), m_proxyPort);
+        sprintf(proxyurl, "%s:%d", m_proxyAddr.c_str(), m_proxyPort);
         m_pSocket->SetProxy(proxyurl);
         delete [] proxyurl;
     }
