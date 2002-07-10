616c60fed7591edb0d24005b20b820ba1ad2be1f
Robert Kaye <rob@musicbrainz.org>
2002-07-10 21:32:58 +0000

    Adding the necessary win32 ifdefs
    git-svn-id: http://svn.musicbrainz.org/libmusicbrainz/trunk@526 b0b80210-5d09-0410-99dd-b4bd03f891c0

--- examples/findalbum.c
+++ examples/findalbum.c
@@ -40,6 +40,10 @@ int main(int argc, char *argv[])
     // Create the musicbrainz object, which will be needed for subsequent calls
     o = mb_New();
 
+#ifdef WIN32
+    mb_WSAInit(o);
+#endif
+
     // Tell the client library to return data in ISO8859-1 and not UTF-8
     mb_UseUTF8(o, 0);
 
@@ -108,6 +112,10 @@ int main(int argc, char *argv[])
 
         printf("\n");
     }
+
+#ifdef WIN32
+    mb_WSAInit(o);
+#endif
 
     // and clean up the musicbrainz object
     mb_Delete(o);
