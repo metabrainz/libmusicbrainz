8dfe3621c526399f089d92d4faa6c2bac78d9c03
Robert Kaye <rob@musicbrainz.org>
2003-04-01 02:31:35 +0000

    Merging in the OSX branch

--- autogen.sh
+++ autogen.sh
@@ -9,12 +9,25 @@ test -z "$srcdir" && srcdir=.
 
 cd "$srcdir"
 DIE=0
+LIBTOOLIZE=libtoolize
+
+# Check to see if we need to use the Mac OS X libtool version
+(glibtoolize --version) < /dev/null > /dev/null 2>&1 && {
+        LIBTOOLIZE=glibtoolize
+}
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
         echo
         echo "You must have autoconf installed to compile $package."
         echo "Download the appropriate package for your distribution,"
-        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/autoconf"
+        DIE=1
+}
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+        echo
+        echo "You must have automake installed to compile $package."
+        echo "Download the appropriate package for your distribution,"
+        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/automake"
         DIE=1
 }
 
@@ -31,8 +44,8 @@ echo "  aclocal $ACLOCAL_FLAGS"
 aclocal $ACLOCAL_FLAGS
 echo "  autoheader"
 autoheader
-echo "  libtoolize --automake --force"
-libtoolize --automake --force
+echo "  $LIBTOOLIZE --automake --force"
+$LIBTOOLIZE --automake --force
 echo "  automake --add-missing"
 automake --add-missing 
 echo "  autoconf"
