b606731742b1e4db004c932bac319f35727734ac
Robert Kaye <rob@musicbrainz.org>
2004-05-19 19:56:54 +0000

    Fixed a C++ vs C issue.

--- examples/getalbum.c
+++ examples/getalbum.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 {
     musicbrainz_t o;
     char          error[256], data[256], temp[256], *args[2];
-    int           ret, numTracks, trackNum, i, isMultipleArtist = 0;
+    int           ret, numTracks, trackNum, i, isMultipleArtist = 0, numDates;
 
     if (argc < 2)
     {
@@ -141,7 +141,7 @@ int main(int argc, char *argv[])
     if (mb_GetResultData(o, MBE_AlbumGetAmazonCoverartURL, data, 256))
        printf("CoverartURL: %s\n", data);
 
-    int numDates = mb_GetResultInt(o, MBE_AlbumGetNumReleaseDates);
+    numDates = mb_GetResultInt(o, MBE_AlbumGetNumReleaseDates);
     for(i = 1; i <= numDates; i++)
     {
         // Select the first release date
