2324385d63b0f30aeda75404beebee9611d4ea54
Lukáš Lalinský <lalinsky@gmail.com>
2006-05-27 00:09:30 +0000

    Updated documentation, fixed formatting.

--- lib/artistalias.cpp
+++ lib/artistalias.cpp
@@ -2,7 +2,7 @@
  * MusicBrainz -- The Internet music metadatabase
  *
  * Copyright (C) 2006 Lukas Lalinsky
- *  
+ *	
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -10,60 +10,57 @@
  * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
  * Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA	 02111-1307	 USA
  */
  
-#include <string>
 #include <musicbrainz3/artistalias.h>
 
 using namespace std;
 using namespace MusicBrainz;
 
 ArtistAlias::ArtistAlias(const string &value, const string &type, const string &script)
+	: value(value), type(type), script(script)
 {
-    setValue(value);
-    setType(type);
-    setScript(script);
 }
 
 string
 ArtistAlias::getType() const
 {
-    return type;
+	return type;
 }
 
 void
 ArtistAlias::setType(const string &value)
 {
-    type = value;
+	type = value;
 }
 
 string
 ArtistAlias::getValue() const
 {
-    return value;
+	return value;
 }
 
 void
 ArtistAlias::setValue(const string &value_)
 {
-    value = value_;
+	value = value_;
 }
 
 string
 ArtistAlias::getScript() const
 {
-    return script;
+	return script;
 }
 
 void
 ArtistAlias::setScript(const string &value)
 {
-    script = value;
+	script = value;
 }
 
