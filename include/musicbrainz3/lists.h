7e2005c0d572b9cda0ddc830671ce91cf413d951
Lukáš Lalinský <lalinsky@gmail.com>
2006-05-27 16:50:47 +0000

    Documentation, formatting.

--- include/musicbrainz3/lists.h
+++ include/musicbrainz3/lists.h
@@ -2,7 +2,7 @@
  * MusicBrainz -- The Internet music metadatabase
  *
  * Copyright (C) 2006 Lukas Lalinsky
- *	
+ *  
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -10,18 +10,16 @@
  * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA	 02111-1307	 USA
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
  
 #ifndef __MUSICBRAINZ3_LISTS_H__
 #define __MUSICBRAINZ3_LISTS_H__
-
-#include <vector>
 
 namespace MusicBrainz
 {
@@ -38,18 +36,30 @@ namespace MusicBrainz
 	class TrackResult;
 	class User;
 	
+	//! A vector of pointers to Artist objects
 	typedef std::vector<Artist *> ArtistList;
+	//! A vector of pointers to ArtistAlias objects
 	typedef std::vector<ArtistAlias *> ArtistAliasList;
+	//! A vector of pointers to ArtistResult objects
 	typedef std::vector<ArtistResult *> ArtistResultList;
+	//! A vector of pointers to Disc objects
 	typedef std::vector<Disc *> DiscList;
+	//! A vector of pointers to Relation objects
 	typedef std::vector<Relation *> RelationList;
+	//! A vector of pointers to Release objects
 	typedef std::vector<Release *> ReleaseList;
+	//! A vector of pointers to ReleaseEvent objects
 	typedef std::vector<ReleaseEvent *> ReleaseEventList;
+	//! A vector of pointers to ReleaseResult objects
 	typedef std::vector<ReleaseResult *> ReleaseResultList;
+	//! A vector of pointers to Track objects
 	typedef std::vector<Track *> TrackList;
+	//! A vector of pointers to TrackResult objects
 	typedef std::vector<TrackResult *> TrackResultList;
+	//! A vector of pointers to User objects
 	typedef std::vector<User *> UserList;
 	
 }
 
 #endif
+
--- include/musicbrainz3/metadata.h
+++ include/musicbrainz3/metadata.h
@@ -26,52 +26,109 @@
 
 namespace MusicBrainz
 {
-    
-    /**
-     * Represents a parsed Music Metadata XML document.
-     *
-     * The Music Metadata XML format is very flexible and may contain a
-     * diverse set of data (e.g. an artist, a release and a list of tracks),
-     * but usually only a small subset is used (either an artist, a release
-     * or a track, or a lists of objects from one class).
-     *
-     * @see MbXmlParser for reading
-     */
-    class MB_API Metadata 
-    {
-    public:
-    
-        Metadata();
+	
+	/**
+	 * Represents a parsed Music Metadata XML document.
+	 *
+	 * The Music Metadata XML format is very flexible and may contain a
+	 * diverse set of data (e.g. an artist, a release and a list of tracks),
+	 * but usually only a small subset is used (either an artist, a release
+	 * or a track, or a lists of objects from one class).
+	 *
+	 * @see MbXmlParser for reading
+	 */
+	class MB_API Metadata 
+	{
+	public:
+	
+		Metadata();
 		
 		virtual ~Metadata();
-        
-        Artist *getArtist() const;
-        void setArtist(Artist *artist);
-        
-        Release *getRelease() const;
-        void setRelease(Release *release);
-        
-        Track *getTrack() const;
-        void setTrack(Track *track);
-        
+		
+		/**
+		 * Returns an artist.
+		 *
+		 * @return a pointer to Artist object, or NULL
+		 */
+		Artist *getArtist() const;
+		
+		/**
+		 * Sets the artist.
+		 *
+		 * @param track a pointer to Artist object, or NULL
+		 */
+		void setArtist(Artist *artist);
+		
+		/**
+		 * Returns a release.
+		 *
+		 * @return a pointer to Release object, or NULL
+		 */
+		Release *getRelease() const;
+		
+		/**
+		 * Sets the release.
+		 *
+		 * @param track a pointer to Release object, or NULL
+		 */
+		void setRelease(Release *release);
+		
+		/**
+		 * Returns a track.
+		 *
+		 * @return a pointer to Track object, or NULL
+		 */
+		Track *getTrack() const;
+		
+		/**
+		 * Sets the track.
+		 *
+		 * @param track a pointer to Track object, or NULL
+		 */
+		void setTrack(Track *track);
+		
+		/**
+		 * Returns a list of users.
+		 *
+		 * @return a vector of pointers to User objects
+		 *
+		 * @note This is a MusicBrainz extension.
+		 */
 		UserList &getUserList();
 		
+		/**
+		 * Returns a artist result list. 
+		 *
+		 * @return a vector of pointers to ArtistResult objects
+		 */
 		ArtistResultList &getArtistResults();
+		
+		/**
+		 * Returns a track result list. 
+		 *
+		 * @return a vector of pointers to TrackResult objects
+		 */
 		TrackResultList &getTrackResults();
+		
+		/**
+		 * Returns a release result list. 
+		 *
+		 * @return a vector of pointers to ReleaseResult objects
+		 */
 		ReleaseResultList &getReleaseResults();
 		
-    private:
+	private:
 	
-        Artist *artist;
-        Release *release;
-        Track *track;
+		Artist *artist;
+		Release *release;
+		Track *track;
 		UserList userList;
 		ArtistResultList artistResults;
 		TrackResultList trackResults;
 		ReleaseResultList releaseResults;
-		
-    };
-    
+	};
+	
 }
 
 #endif
+
