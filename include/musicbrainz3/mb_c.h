0f01a07f23b947ebd1d02dac8242d9df3c711a9e
Lukáš Lalinský <lalinsky@gmail.com>
2006-05-31 12:18:27 +0000

    C bindings for Includes.

--- include/musicbrainz3/mb_c.h
+++ include/musicbrainz3/mb_c.h
@@ -47,6 +47,10 @@ typedef void *MBTrack;
 typedef void *MBRelease;
 typedef void *MBUser;
 
+typedef void *MBArtistIncludes;
+typedef void *MBReleaseIncludes;
+typedef void *MBTrackIncludes;
+
 /* === MusicBrainz::WebService === */
 
 MB_API void
@@ -61,15 +65,18 @@ MB_API void
 mb_query_free(MBQuery query);
 
 MB_API MBArtist
-mb_query_get_artist_by_id(MBQuery query, const char *id);
+mb_query_get_artist_by_id(MBQuery query, const char *id, MBArtistIncludes inc);
 
 MB_API MBRelease
-mb_query_get_release_by_id(MBQuery query, const char *id);
+mb_query_get_release_by_id(MBQuery query, const char *id, MBReleaseIncludes inc);
 
 MB_API MBTrack
-mb_query_get_track_by_id(MBQuery query, const char *id);
+mb_query_get_track_by_id(MBQuery query, const char *id, MBTrackIncludes inc);
 
 /* === MusicBrainz::Artist === */
+
+MB_API void
+mb_artist_free(MBArtist artist);
 
 MB_API int
 mb_artist_get_id(MBArtist artist, char *str, int len);
@@ -91,6 +98,9 @@ mb_artist_get_end_date(MBArtist artist, char *str, int len);
 
 /* === MusicBrainz::Release === */
 
+MB_API void
+mb_release_free(MBRelease release);
+
 MB_API int
 mb_release_get_id(MBRelease release, char *str, int len);
 
@@ -107,6 +117,9 @@ MB_API int
 mb_release_get_asin(MBRelease release, char *str, int len);
 
 /* === MusicBrainz::Track === */
+
+MB_API void
+mb_track_free(MBTrack track);
 
 MB_API int
 mb_track_get_id(MBTrack track, char *str, int len);
@@ -130,11 +143,107 @@ mb_artist_alias_get_script(MBArtistAlias alias, char *str, int len);
 
 /* === MusicBrainz::User === */
 
+MB_API void
+mb_user_free(MBUser user);
+
 MB_API int
 mb_user_get_name(MBUser user, char *str, int len);
 
 MB_API int
 mb_user_get_show_nag(MBUser user);
+
+/* === MusicBrainz::ArtistIncludes === */
+
+MB_API MBArtistIncludes
+mb_artist_includes_new();
+
+MB_API void
+mb_artist_includes_free(MBArtistIncludes inc);
+
+MB_API MBArtistIncludes
+mb_artist_includes_aliases(MBArtistIncludes inc);
+
+MB_API MBArtistIncludes
+mb_artist_includes_releases(MBArtistIncludes inc, const char *type);
+
+MB_API MBArtistIncludes
+mb_artist_includes_va_releases(MBArtistIncludes inc, const char *type);
+
+MB_API MBArtistIncludes
+mb_artist_includes_artist_relations(MBArtistIncludes inc);
+
+MB_API MBArtistIncludes
+mb_artist_includes_release_relations(MBArtistIncludes inc);
+
+MB_API MBArtistIncludes
+mb_artist_includes_track_relations(MBArtistIncludes inc);
+
+MB_API MBArtistIncludes
+mb_artist_includes_url_relations(MBArtistIncludes inc);
+
+/* === MusicBrainz::ReleaseIncludes === */
+
+MB_API MBReleaseIncludes
+mb_release_includes_new();
+
+MB_API void
+mb_release_includes_free(MBReleaseIncludes inc);
+
+MB_API MBReleaseIncludes
+mb_release_includes_artist(MBReleaseIncludes inc);
+
+MB_API MBReleaseIncludes
+mb_release_includes_counts(MBReleaseIncludes inc);
+
+MB_API MBReleaseIncludes
+mb_release_includes_release_events(MBReleaseIncludes inc);
+
+MB_API MBReleaseIncludes
+mb_release_includes_discs(MBReleaseIncludes inc);
+
+MB_API MBReleaseIncludes
+mb_release_includes_tracks(MBReleaseIncludes inc);
+
+MB_API MBReleaseIncludes
+mb_release_includes_artist_relations(MBReleaseIncludes inc);
+
+MB_API MBReleaseIncludes
+mb_release_includes_release_relations(MBReleaseIncludes inc);
+
+MB_API MBReleaseIncludes
+mb_release_includes_track_relations(MBReleaseIncludes inc);
+
+MB_API MBReleaseIncludes
+mb_release_includes_url_relations(MBReleaseIncludes inc);
+
+/* === MusicBrainz::TrackIncludes === */
+
+MB_API MBTrackIncludes
+mb_track_includes_new();
+
+MB_API void
+mb_track_includes_free(MBArtistIncludes inc);
+
+MB_API MBTrackIncludes
+mb_track_includes_artist(MBArtistIncludes inc);
+
+MB_API MBTrackIncludes
+mb_track_includes_releases(MBArtistIncludes inc);
+
+MB_API MBTrackIncludes
+mb_track_includes_puids(MBArtistIncludes inc);
+
+MB_API MBTrackIncludes
+mb_track_includes_artist_relations(MBArtistIncludes inc);
+
+MB_API MBTrackIncludes
+mb_track_includes_release_relations(MBTrackIncludes inc);
+
+MB_API MBTrackIncludes
+mb_track_includes_track_relations(MBTrackIncludes inc);
+
+MB_API MBTrackIncludes
+mb_track_includes_url_relations(MBTrackIncludes inc);
 
 #ifdef __cplusplus
 }
