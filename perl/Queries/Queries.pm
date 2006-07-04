cbf4ca035105e902bc70e52080e7170e3fae01c1
svanzoest <svanzoest@musicbrainz.org>
2006-07-04 19:45:43 +0000

    Updated for new release with fixed MANIFEST
    git-svn-id: http://svn.musicbrainz.org/libmusicbrainz/trunk@8095 b0b80210-5d09-0410-99dd-b4bd03f891c0

--- perl/Queries/Queries.pm
+++ perl/Queries/Queries.pm
@@ -138,7 +138,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw();
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
