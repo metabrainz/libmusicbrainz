9b5b4a0e83ba6932875ae7f6bb59b4b1ef89e64f
Lukáš Lalinský <lalinsky@gmail.com>
2008-04-22 08:26:21 +0000

    Fix compilation on Solaris 10u4. (closes #3364, patch by Mark Wright)

--- osdep/mb_solaris.cpp
+++ osdep/mb_solaris.cpp
@@ -31,8 +31,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <sys/byteorder.h>
 #include <sys/types.h>
+#include <sys/byteorder.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <unistd.h>         // ioctl()
