b21215a9772001e84901ead5e119eef66745cfc5
Andy Hawkins <andy@gently.org.uk>
2011-06-23 15:41:32 +0000

    Compile tests for C Interface - CMetadata

--- examples/ctest.c
+++ examples/ctest.c
@@ -71,47 +71,35 @@ void CompileTest()
 	Mb4Alias Alias=0;
 	Mb4AliasList AliasList=0;
 	Mb4Annotation Annotation=0;
-/*
 	Mb4AnnotationList AnnotationList=0;
-*/
 	Mb4Artist Artist=0;
 	Mb4ArtistCredit ArtistCredit=0;
-/*
 	Mb4ArtistList ArtistList=0;
-*/
 	Mb4Attribute Attribute=0;
 /*
 	Mb4AttributeList AttributeList=0;
 */
 	Mb4CDStub CDStub=0;
-/*
 	Mb4CDStubList CDStubList=0;
-*/
 	Mb4Collection Collection=0;
-/*
 	Mb4CollectionList CollectionList=0;
-*/
 	Mb4Disc Disc=0;
 	Mb4DiscList DiscList=0;
 	Mb4FreeDBDisc FreeDBDisc=0;
-/*
 	Mb4FreeDBDiscList FreeDBDiscList=0;
-*/
 	Mb4ISRC ISRC=0;
-/*
 	Mb4ISRCList ISRCList=0;
-*/
 	Mb4Label Label=0;
 	Mb4LabelInfo LabelInfo=0;
-/*
 	Mb4LabelInfoList LabelInfoList=0;
-*/
 	Mb4LabelList LabelList=0;
 	Mb4Lifespan Lifespan=0;
 	Mb4Medium Medium=0;
 /*
 	Mb4MediumList MediumList=0;
+*/
 	Mb4Metadata Metadata=0;
+/*
 	Mb4NameCredit NameCredit=0;
 */
 	Mb4NameCreditList NameCreditList=0;
@@ -119,25 +107,23 @@ void CompileTest()
 	Mb4NoneMBTrack NoneMBTrack=0;
 */
 	Mb4NoneMBTrackList NoneMBTrackList=0;
-/*
 	Mb4PUID PUID=0;
+/*
 	Mb4PUIDList PUIDList=0;
 	Mb4Query Query=0;
 */
 	Mb4Rating Rating=0;
 /*
 	Mb4RatingList RatingList=0;
-	Mb4Recording Recording=0;
 */
+	Mb4Recording Recording=0;
 	Mb4RecordingList RecordingList=0;
 /*
 	Mb4Relation Relation=0;
 */
 	Mb4RelationList RelationList=0;
-/*
 	Mb4Release Release=0;
 	Mb4ReleaseGroup ReleaseGroup=0;
-*/
 	Mb4ReleaseGroupList ReleaseGroupList=0;
 	Mb4ReleaseList ReleaseList=0;
 /*
@@ -154,9 +140,7 @@ void CompileTest()
 	Mb4UserTag UserTag=0;
 */
 	Mb4UserTagList UserTagList=0;
-/*
 	Mb4Work Work=0;
-*/
 	Mb4WorkList WorkList=0;
 	char Str[256];
 	int Size=sizeof(Str);
@@ -267,6 +251,37 @@ void CompileTest()
 	TrackList=mb4_medium_get_tracklist(Medium);
 	DummyBool=mb4_medium_contains_discid(Medium,"1234");
 	mb4_medium_delete(Medium);
+
+	mb4_metadata_get_generator(Metadata,Str,Size);
+	mb4_metadata_get_created(Metadata,Str,Size);
+	Artist=mb4_metadata_get_artist(Metadata);
+	Release=mb4_metadata_get_release(Metadata);
+	ReleaseGroup=mb4_metadata_get_releasegroup(Metadata);
+	Recording=mb4_metadata_get_recording(Metadata);
+	Label=mb4_metadata_get_label(Metadata);
+	Work=mb4_metadata_get_work(Metadata);
+	PUID=mb4_metadata_get_puid(Metadata);
+	ISRC=mb4_metadata_get_isrc(Metadata);
+	Disc=mb4_metadata_get_disc(Metadata);
+	LabelInfoList=mb4_metadata_get_labelinfolist(Metadata);
+	Rating=mb4_metadata_get_rating(Metadata);
+	UserRating=mb4_metadata_get_userrating(Metadata);
+	Collection=mb4_metadata_get_collection(Metadata);
+	ArtistList=mb4_metadata_get_artistlist(Metadata);
+	ReleaseList=mb4_metadata_get_releaselist(Metadata);
+	ReleaseGroupList=mb4_metadata_get_releasegrouplist(Metadata);
+	RecordingList=mb4_metadata_get_recordinglist(Metadata);
+	LabelList=mb4_metadata_get_labellist(Metadata);
+	WorkList=mb4_metadata_get_worklist(Metadata);
+	ISRCList=mb4_metadata_get_isrclist(Metadata);
+	AnnotationList=mb4_metadata_get_annotationlist(Metadata);
+	CDStubList=mb4_metadata_get_cdstublist(Metadata);
+	FreeDBDiscList=mb4_metadata_get_freedbdisclist(Metadata);
+	TagList=mb4_metadata_get_taglist(Metadata);
+	UserTagList=mb4_metadata_get_usertaglist(Metadata);
+	CollectionList=mb4_metadata_get_collectionlist(Metadata);
+	CDStub=mb4_metadata_get_cdstub(Metadata);
+	mb4_metadata_delete(Metadata);
 
 	mb4_alias_list_size(AliasList);
 	Alias=mb4_alias_list_item(AliasList,0);
