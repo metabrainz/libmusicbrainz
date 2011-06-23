1dc187684ab1fbbf77a81ecfe6b6529ffc4f108e
Andy Hawkins <andy@gently.org.uk>
2011-06-23 10:32:39 +0000

    Import

--- libmusicbrainz4.pc.cmake
+++ libmusicbrainz4.pc.cmake
@@ -8,6 +8,6 @@ Description: The Musicbrainz Client Library.
 URL: http://musicbrainz.org/doc/libmusicbrainz
 Version: ${PROJECT_VERSION}
 Requires.private: neon >= 0.25, libdiscid
-Libs: -L${LIB_INSTALL_DIR} -lmusicbrainz3
+Libs: -L${LIB_INSTALL_DIR} -lmusicbrainz4
 Cflags: -I${INCLUDE_INSTALL_DIR}
 
