ef8deae7c758a46f78ef87a11a99460272cea718
Andy Hawkins <andy@gently.org.uk>
2012-09-03 21:28:15 +0100

    Correct inconsistent licensing text

--- src/LabelInfo.cc
+++ src/LabelInfo.cc
@@ -7,8 +7,9 @@
    This file is part of libmusicbrainz5.
 
    This library is free software; you can redistribute it and/or
-   modify it under the terms of v2 of the GNU Lesser General Public
-   License as published by the Free Software Foundation.
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
    libmusicbrainz5 is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
